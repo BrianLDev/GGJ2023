e32461dba9449775f4e978d636e8278e14fb9ebc
asathkumara <28933557+asathkumara@users.noreply.github.com>
2023-02-05 09:13:48 -0800

    Added functions to reduce shield, health, and ammo bars

--- Assets/Scripts/UI/Menus/GameMenu.cs
+++ Assets/Scripts/UI/Menus/GameMenu.cs
@@ -12,10 +12,19 @@ public class GameMenu : MonoBehaviour
 {
     private const string _resumeButtonID = "resume__button";
     private const string _exitButtonID = "exit__button";
+    private const string _playerShieldBarID = "playerShieldBar";
+    private const string _playerHealthBarID = "playerHealthBar";
+    private const string _playerAmmoBarID = "playerAmmoBar";
+    private const int _barSize = 200;
 
     private UIDocument _gameDocument;
     private VisualElement _root;
     private VisualElement _pauseMenuRef;
+    private VisualElement _gameWonMenuRef;
+    private VisualElement _gameLostMenuRef;
+    private VisualElement _playerHealthBarRef;
+    private VisualElement _playerShieldBarRef;
+    private VisualElement _playerAmmoBarRef;
     private Button _resumeButtonRef;
     private Button _exitButtonRef;
 
@@ -26,6 +35,8 @@ public class GameMenu : MonoBehaviour
     [Tooltip("String IDs to query Visual Elements")] 
     [SerializeField]
     string PauseMenuID = "PauseScreen";
+    string GameWonMenuID = "GameWon";
+    string GameLostMenuID = "GameLost";
 
     [Header("Blur")] 
     [Tooltip("Volume used to blur")] 
@@ -35,11 +46,14 @@ public class GameMenu : MonoBehaviour
     public static event Action GamePaused;
     public static event Action GameResumed;
     public static event Action GameRestarted;
+    public static event Action GameWon;
+    public static event Action GameLost;
 
     void OnEnable()
     {
         SetVisualElements();
         RegisterButtonCallbacks();
+        InitializePlayerBars();
 
         if (Volume == null)
             Volume = FindObjectOfType<Volume>();
@@ -58,6 +72,25 @@ public class GameMenu : MonoBehaviour
             Debug.Log("GameMenu.cs: Escape key pressed");
             ShowPauseMenu();
         }
+
+        if (Input.GetKeyDown("m"))
+        {
+            Debug.Log("GameMenu.cs: M Pressed");
+            GainShield(20);
+        }
+
+        if (Input.GetKeyDown("k"))
+        {
+            Debug.Log("GameMenu.cs: K Pressed");
+            ReduceHealth(10);
+
+        }
+
+        if (Input.GetKeyDown("l"))
+        {
+            Debug.Log("GameMenu.cs: L Pressed");
+            GainHealth(10);
+        }
     }
 
     /// <summary>
@@ -70,8 +103,14 @@ public class GameMenu : MonoBehaviour
         _root = _gameDocument.rootVisualElement;
 
         _pauseMenuRef = _root.Query(PauseMenuID);
+        _pauseMenuRef = _root.Query(PauseMenuID);
+        _pauseMenuRef = _root.Query(PauseMenuID);
         _resumeButtonRef = _root.Query<Button>(_resumeButtonID);
         _exitButtonRef = _root.Query<Button>(_exitButtonID);
+
+        _playerShieldBarRef = _root.Query<VisualElement>(_playerShieldBarID);
+        _playerHealthBarRef = _root.Query<VisualElement>(_playerHealthBarID);
+        _playerAmmoBarRef = _root.Query<VisualElement>(_playerAmmoBarID);
     }
 
     /// <summary>
@@ -83,6 +122,129 @@ public class GameMenu : MonoBehaviour
         _exitButtonRef?.RegisterCallback<ClickEvent>(ExitGame);
     }
 
+    /// <summary>
+    /// Initializes the player bars
+    /// </summary>
+    /// <remarks>Shield is hidden and width is set to the bar size constant</remarks>
+    void InitializePlayerBars()
+    {
+        _playerShieldBarRef.style.width = new StyleLength(new Length(0, LengthUnit.Pixel));
+        _playerHealthBarRef.style.width = new StyleLength(new Length(_barSize, LengthUnit.Pixel));
+        _playerAmmoBarRef.style.width = new StyleLength(new Length(_barSize, LengthUnit.Pixel));
+    }
+
+    /// <summary>
+    /// Increases the health bar
+    /// </summary>
+    /// <param name="healthReceived">The health received</param>
+    public void GainHealth(int healthReceived)
+    {
+        Debug.Log($"GameMenu.cs: Health gained {healthReceived}");
+        float healthBarValue = _playerHealthBarRef.style.width.value.value;
+
+        if ((healthBarValue + healthReceived) >= _barSize)
+        {
+            _playerHealthBarRef.style.width = new StyleLength(new Length(_barSize, LengthUnit.Pixel));
+            return;
+        }
+
+        _playerHealthBarRef.style.width = new StyleLength(new Length(healthBarValue + healthReceived, LengthUnit.Pixel));
+    }
+
+    /// <summary>
+    /// Increases the shield bar
+    /// </summary>
+    /// <param name="shieldGained">The shield gained</param>
+    public void GainShield(int shieldGained)
+    {
+        Debug.Log($"GameMenu.cs: Shield gained {shieldGained}");
+        float shieldBarValue = _playerShieldBarRef.style.width.value.value;
+
+        if ((shieldBarValue + shieldGained) >= _barSize)
+        {
+            _playerShieldBarRef.style.width = new StyleLength(new Length(_barSize, LengthUnit.Pixel));
+            return;
+        }
+
+        _playerShieldBarRef.style.width = new StyleLength(new Length(shieldBarValue + shieldGained, LengthUnit.Pixel));
+    }
+
+    /// <summary>
+    /// Reduces the health bar and shield bar as needed
+    /// </summary>
+    /// <param name="damageTaken">The damage taken</param>
+    /// <remarks>If a shield is active, the shield is reduced instead of the health.
+    /// Once the health reaches 0, the game over menu is shown.
+    /// </remarks>
+    public void ReduceHealth(int damageTaken)
+    {
+        Debug.Log($"GameMenu.cs: Health reduced {damageTaken}");
+        float shieldBarValue = _playerShieldBarRef.style.width.value.value;
+        float healthBarValue  = _playerHealthBarRef.style.width.value.value;
+
+        VisualElement barToReduceRef = _playerHealthBarRef;
+        float barValueToCompare = healthBarValue;
+
+        if (healthBarValue <= 0)
+        {
+            ShowGameLostMenu();
+            return;
+        }
+
+        if (shieldBarValue > 0)
+        {
+            barToReduceRef = _playerShieldBarRef;
+            barValueToCompare = shieldBarValue;
+        }
+
+        if ((barValueToCompare - damageTaken) <= 0)
+        {
+            barToReduceRef.style.width = new StyleLength(new Length(0, LengthUnit.Pixel));
+            return;
+        }
+
+        barToReduceRef.style.width = new StyleLength(new Length(barValueToCompare - damageTaken, LengthUnit.Pixel));
+
+    }
+
+    
+    /// <summary>
+    /// Increases the ammo bar
+    /// </summary>
+    /// <param name="ammo">The ammo received</param>
+    public void GainAmmo(int ammo)
+    {
+        Debug.Log($"GameMenu.cs: Ammo gained {ammo}");
+        float ammoBarValue = _playerAmmoBarRef.style.width.value.value;
+
+        if ((ammoBarValue + ammo) >= _barSize)
+        {
+            _playerAmmoBarRef.style.width = new StyleLength(new Length(_barSize, LengthUnit.Pixel));
+            return;
+        }
+
+        _playerAmmoBarRef.style.width = new StyleLength(new Length(ammoBarValue + ammo, LengthUnit.Pixel));
+    }
+
+    /// <summary>
+    /// Reduces the ammo bar
+    /// </summary>
+    /// <param name="ammoExpended">The ammo expended</param>
+    public void ReduceAmmo(int ammoExpended)
+    {
+        Debug.Log($"GameMenu.cs: Ammo expended {ammoExpended}");
+        float ammoBarValue = _playerAmmoBarRef.style.width.value.value;
+
+
+        if ((ammoBarValue - ammoExpended) <= 0)
+        {
+            _playerAmmoBarRef.style.width = new StyleLength(new Length(0, LengthUnit.Pixel));
+            return;
+        }
+
+        _playerAmmoBarRef.style.width = new StyleLength(new Length(ammoBarValue - ammoExpended, LengthUnit.Pixel));
+    }
+    
     /// <summary>
     /// Shows a visual element
     /// </summary>
@@ -105,6 +267,30 @@ public class GameMenu : MonoBehaviour
         GamePaused?.Invoke();
 
         ShowVisualElement(_pauseMenuRef, true);
+        BlurBackground(true);
+    }
+
+    /// <summary>
+    /// Shows Game Won Menu
+    /// </summary>
+    void ShowGameWonMenu()
+    {
+        Debug.Log("GameMenu.cs: Game Won");
+        GameWon?.Invoke();
+
+        ShowVisualElement(_gameWonMenuRef, true);
+        BlurBackground(true);
+    }
+
+    /// <summary>
+    /// Shows Game Lost Menu
+    /// </summary>
+    void ShowGameLostMenu()
+    {
+        Debug.Log("GameMenu.cs: Game Won");
+        GameLost?.Invoke();
+
+        ShowVisualElement(_gameLostMenuRef, true);
         BlurBackground(true);
     }
 
