5b70dd58c5a69dd171784ac3c6507ea4b2cf7f42
Brian Leip <brianleip@gmail.com>
2023-02-04 19:16:02 -0800

    Simple AI for crab enemy

--- Assets/Scripts/Enemies/Crab.cs
+++ Assets/Scripts/Enemies/Crab.cs
@@ -3,14 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Crab : EnemyBase {
+  public enum CrabState { Idle, WalkLeft, WalkRight }
+
   [SerializeField] private float damage;
+  [SerializeField] private float walkSpeed = 0.5f;
+  [SerializeField] private float stateDuration = 3f;
+  private CrabState crabState = CrabState.Idle;
+  private float stateTimer = 0;
+  private Rigidbody2D rb;
+  private Animator animator;
+  private SpriteRenderer spriteRenderer;
+
+  protected void Awake() {
+    rb = GetComponent<Rigidbody2D>();
+    animator = GetComponentInChildren<Animator>();
+    spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+  }
 
   protected new void Start() {
       base.Start();
   }
 
   protected void Update() {
-    // TODO: MOVEMENT LOGIC AND SIMPLE AI
+    // choose state
+    stateTimer -= Time.deltaTime;
+    if (stateTimer <= 0) {
+      stateTimer = stateDuration * Random.Range(0f, 1f);
+      int random = Random.Range(0, 3);
+      ChangeStates((CrabState)random);
+    }
+  }
+
+  protected void FixedUpdate() {
+    // handle movement
+    if (crabState == CrabState.WalkLeft)
+      rb.velocity = Vector3.left * walkSpeed;
+    else if (crabState == CrabState.WalkRight)
+      rb.velocity = Vector3.right * walkSpeed;
+  }
+
+  protected void ChangeStates(CrabState newState) {
+    crabState = newState;
+    if (crabState == CrabState.Idle) {
+      rb.velocity = Vector3.zero;
+      animator.SetBool("isWalking", false);
+    }
+    else if (crabState == CrabState.WalkLeft) {
+      spriteRenderer.flipX = false;
+      animator.SetBool("isWalking", true);
+    }
+    else if (crabState == CrabState.WalkRight) {
+      spriteRenderer.flipX = true;
+      animator.SetBool("isWalking", true);
+    }
   }
 
   protected override void Die() {
