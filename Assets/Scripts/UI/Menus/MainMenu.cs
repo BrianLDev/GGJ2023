0d218827089d1ca32a74bdef35a526d8dca72bea
asathkumara <28933557+asathkumara@users.noreply.github.com>
2023-02-05 17:16:27 -0800

    Added logic to show credits menu when implemented

--- Assets/Scripts/UI/Menus/MainMenu.cs
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -10,16 +10,30 @@ public class MainMenu : Menu
 {
     private const string _startButtonID = "start__button";
     private const string _exitGameButtonID = "exit__button";
+    private const string _creditsButtonID = "credits__button";
+
+    private VisualElement _creditsMenuRef;
 
     private Button _startButtonRef;
     private Button _exitGameButtonRef;
+    private Button _creditsButtonRef;
+
+    [Header("Game Menus")]
+    [Tooltip("String IDs to query Visual Elements")]
+    [SerializeField]
+    string CreditsMenuID = "CreditsMenu";
+
+
 
     protected override void SetVisualElements()
     {
         base.SetVisualElements();
 
+        _creditsMenuRef = _root.Query<VisualElement>(CreditsMenuID);
+
         _startButtonRef = _root.Query<Button>(_startButtonID);
         _exitGameButtonRef = _root.Query<Button>(_exitGameButtonID);
+        _creditsButtonRef = _root.Query<Button>(_creditsButtonID);
     }
 
     protected override void RegisterButtonCallbacks()
@@ -29,6 +43,7 @@ public class MainMenu : Menu
         // register action when each button is clicked
         _startButtonRef?.RegisterCallback<ClickEvent>(StartGame);
         _exitGameButtonRef?.RegisterCallback<ClickEvent>(ExitGame);
+        _creditsMenuRef?.RegisterCallback<ClickEvent>(ShowCredits);
     }
 
     /// <summary>
@@ -37,7 +52,7 @@ public class MainMenu : Menu
     /// <param name="clickEvent">The click event object</param>
     void StartGame(ClickEvent clickEvent)
     {
-        Debug.Log("MainMenu: StartGame called, loading scene 'Test'");
+        Debug.Log("MainMenu.cs: StartGame called, loading scene 'Test'");
         SceneManager.LoadScene(1);
     }
 
@@ -47,11 +62,21 @@ public class MainMenu : Menu
     /// <param name="clickEvent">The click event object</param>
     void ExitGame(ClickEvent clickEvent)
     {
-        Debug.Log("MainMenu: ExitGame called");
+        Debug.Log("MainMenu.cs: ExitGame called");
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
         #else
             Application.Quit();
         #endif
+    }
+
+    /// <summary>
+    /// Shows the credits
+    /// </summary>
+    /// <param name="clickEvent">The click event object</param>
+    void ShowCredits(ClickEvent clickEvent)
+    {
+        Debug.Log("MainMenu.cs: Showing credits");
+        _creditsMenuRef.style.display = DisplayStyle.Flex;
     }
 }
