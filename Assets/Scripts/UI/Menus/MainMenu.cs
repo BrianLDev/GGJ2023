000f7e1788710e6f916e3bf45684fb9c26128075
asathkumara <28933557+asathkumara@users.noreply.github.com>
2023-02-05 17:41:42 -0800

    Added credits menu to main menu

--- Assets/Scripts/UI/Menus/MainMenu.cs
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -11,17 +11,23 @@ public class MainMenu : Menu
     private const string _startButtonID = "start__button";
     private const string _exitGameButtonID = "exit__button";
     private const string _creditsButtonID = "credits__button";
+    private const string _creditsBackButtonID = "creditsBack__button";
 
     private VisualElement _creditsMenuRef;
+    private VisualElement _mainMenuRef;
 
     private Button _startButtonRef;
     private Button _exitGameButtonRef;
     private Button _creditsButtonRef;
+    private Button _creditsBackButtonRef;
 
     [Header("Game Menus")]
     [Tooltip("String IDs to query Visual Elements")]
     [SerializeField]
     string CreditsMenuID = "CreditsMenu";
+
+    [SerializeField]
+    string MainMenuID = "MainMenu";
 
 
 
@@ -30,10 +36,12 @@ public class MainMenu : Menu
         base.SetVisualElements();
 
         _creditsMenuRef = _root.Query<VisualElement>(CreditsMenuID);
+        _mainMenuRef = _root.Query<VisualElement>(MainMenuID);
 
         _startButtonRef = _root.Query<Button>(_startButtonID);
         _exitGameButtonRef = _root.Query<Button>(_exitGameButtonID);
         _creditsButtonRef = _root.Query<Button>(_creditsButtonID);
+        _creditsBackButtonRef = _root.Query<Button>(_creditsBackButtonID);
     }
 
     protected override void RegisterButtonCallbacks()
@@ -43,7 +51,8 @@ public class MainMenu : Menu
         // register action when each button is clicked
         _startButtonRef?.RegisterCallback<ClickEvent>(StartGame);
         _exitGameButtonRef?.RegisterCallback<ClickEvent>(ExitGame);
-        _creditsMenuRef?.RegisterCallback<ClickEvent>(ShowCredits);
+        _creditsButtonRef?.RegisterCallback<ClickEvent>(ShowCredits);
+        _creditsBackButtonRef?.RegisterCallback<ClickEvent>(HideCredits);
     }
 
     /// <summary>
@@ -78,5 +87,17 @@ public class MainMenu : Menu
     {
         Debug.Log("MainMenu.cs: Showing credits");
         _creditsMenuRef.style.display = DisplayStyle.Flex;
+        _mainMenuRef.style.display = DisplayStyle.None;
+    }
+
+    /// <summary>
+    /// Shows the credits
+    /// </summary>
+    /// <param name="clickEvent">The click event object</param>
+    void HideCredits(ClickEvent clickEvent)
+    {
+        Debug.Log("MainMenu.cs: Showing credits");
+        _creditsMenuRef.style.display = DisplayStyle.None;
+        _mainMenuRef.style.display = DisplayStyle.Flex;
     }
 }
